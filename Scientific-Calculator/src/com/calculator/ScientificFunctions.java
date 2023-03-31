7c52b3f4f716625308376b32d71327fc58dd6654
Jayakishan Presingu <jayakishan.presingu@gmail.com>
2023-03-30 19:41:04 -0500

    Scientific-Calculator Project Commit

---
+++ Scientific-Calculator/src/com/calculator/ScientificFunctions.java
@@ -0,0 +1,7 @@
+package com.calculator;
+
+public interface ScientificFunctions {
+    double squareRoot(double operand);
+    double exponentiation(double base, double exponent);
+    double logarithm(double base, double operand);
+}
